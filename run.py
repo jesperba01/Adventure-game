fc14cd712a9dff3f22f03328bd61c64577239945
jesperba01 <>
2024-01-04 15:07:34 +0000

    Add introduction function

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
 import colorama
 from colorama import Fore
 
+
+def introduction():
+    """
+    Display an introduction to the game.
+    """
+    print("Welcome to the Adventure Game!")
+    time.sleep(2)
+    print("You find yourself in a mysterious land.")
+    time.sleep(2)
+    print("Your goal is to reach the treasure at the end of the journey.")
+    time.sleep(2)
